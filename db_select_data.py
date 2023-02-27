53e9cbe242a6fb5d416a2e906431a72cb7901cba
“xymurgist” <pps.jim@gmail.com>
2023-02-27 06:41:28 -0700

    Select data working

--- db_select_data.py
+++ db_select_data.py
@@ -1,20 +1,13 @@
-from sqlalchemy.orm import Session
 from sqlalchemy import select
+from sqlalchemy.orm import Session
 from db_engine import engine as engine
 from db_schema import Weapons as Weapons
 
 session = Session(engine)
-
-# Select data from table
-
 stmt = select(Weapons)
-# stmt = select(Weapons).where(Weapons.weapon_name == "HVM 001")
-result = session.execute(stmt)
-for item in result.scalars():
-    print(item.manufacturer)
-# print(result.all())
+result = session.scalars(stmt)
 
-result2 = session.scalars(stmt)
-for item in result2:
-    print(item.weapon_name)
-# print(result2.all())
+
+def all_weapons_properties():
+    for prop in result:
+        yield prop
